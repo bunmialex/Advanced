2e9145129c6daaa437ed97332c168e0d32d1baec
bunmialex <fabosalex@yahoo.com>
2024-01-31 22:56:02 +0100

    Example Project

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+namespace Advanced.Models
+{
+    public class Department
+    {
+        public long Departmentid { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public IEnumerable<Person>? People { get; set; }
+    }
+}
